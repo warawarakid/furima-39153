7f2e0757bf2a194dc4d1c3483a4f1cb0465dc331
warawarakid <sei.shogun@gmail.com>
2023-08-09 10:11:19 +0900

    モデルの単体テストコード

--- db/schema.rb
+++ db/schema.rb
@@ -13,11 +13,17 @@
 ActiveRecord::Schema.define(version: 2023_08_07_030145) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "first_name_kana", null: false
+    t.string "last_name_kana", null: false
+    t.date "birth_day", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    last_name   { 'やマ田' }
+    first_name  { '太ろウ' }
+    last_name_kana  { 'ヤマダ' }
+    first_name_kana { 'タロウ' }
+    birth_day { Faker::Date.between(from: '1930-01-01', to: '2022-12-31') }
+  end
+end
