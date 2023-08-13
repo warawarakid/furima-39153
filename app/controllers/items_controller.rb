0e4cadac756fb3e88b5d6fe0c78023b783e6d2d4
warawarakid <sei.shogun@gmail.com>
2023-08-13 19:54:31 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,25 @@
 class ItemsController < ApplicationController
+
   def index
+    @items = Item.order("created_at DESC")
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:category_id, :condition_id, :shipping_charge_id, :prefecture_id, :shipping_day_id,:image)
   end
 end
