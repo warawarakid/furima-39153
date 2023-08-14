249c4649c95f31f4d65d0d54fd0fb33e065e977a
warawarakid <sei.shogun@gmail.com>
2023-08-14 13:39:36 +0900

    出品商品の情報をテーブルに保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,6 +20,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:category_id, :condition_id, :shipping_charge_id, :prefecture_id, :shipping_day_id,:image)
+    params.require(:item).permit(:name, :description, :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :shipping_day_id, :price, :image).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -9,13 +9,13 @@ class Item < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :shipping_day
   has_one :order
-  
+
   has_one_attached :image
 
 
-  #空の投稿を保存できないようにする
+  
   validates :name, :description, presence: true
 
-  #ジャンルの選択が「---」の時は保存できないようにする
+  
   validates :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :shipping_day_id, numericality: { other_than: 1, message: "can't be blank" } 
 end
