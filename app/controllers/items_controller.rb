d1f1d905460078e8df6a7b1dee0b1b9843e25b6e
warawarakid <sei.shogun@gmail.com>
2023-08-20 12:58:56 +0900

    商品情報編集機能の編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :ensure_correct_user, only: [:edit, :update, :destroy]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :move_to_index, only: [:edit, :update]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -59,5 +60,11 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    if current_user.id == @item.user_id && @item.order.present?
+      redirect_to root_path
+    end
   end
 end
